e806060bca4a7baae329cc38b23944268cee4909
minibrams <anders@brams.dk>
2018-10-17 15:23:07 +0200

    Graph building is functional and basic keyword extraction works as intended.

--- preprocessing/text_preprocessing.py
+++ preprocessing/text_preprocessing.py
@@ -1,75 +1,26 @@
+import nltk 
+from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import SnowballStemmer
+from nltk.corpus import stopwords
 
-from itertools import islice
+def tokenize(text, stopwords):
+    res = '' 
 
-def read_stopwords(from_file = 'stopwords.txt'):
-    ''' Read stopwords (space separated) from a file and return as a list of strings. '''
-    try:
-        fp = open(from_file)
-        words = fp.read().split(' ')
-        fp.close()
-        return words
-    except FileNotFoundError as err: 
-        print(f'Could not find file: {from_file}.')
-        print(f'Full exception:')
-        print(err)
-        exit(-1)
-
-def get_attribute_vector(tokenized_text, token_set):
-    ''' Converts a list of tokens to a vector of {token : attributes} forms. '''
-    if len(token_set) == 0: 
-        raise Exception('Cannot convert a text to attribute vector without a token set.')
-
-   
-
-def remove_punctuation(text):
-    ret = '' 
+    # Remove punctuation
     for char in text: 
         if char.isalpha() or char == ' ':
-            ret += char 
+            res += char 
 
-    return ret
-
-
-def tokenize(text, stopwords, only_letters = True):
-    ''' Convert a text to a list of tokens.
-        Omits all words in the stopwords parameter.
-        Removes all characters that are punctuation chars 
-        if only_letters is True.
-    '''
-
-    text = text.lower()
-
-    if only_letters: 
-        text = remove_punctuation(text)
-
-    words = text.split(' ')
-    return [word for word in words if (word not in stopwords)]
-
-def get_cooccurence_matrix(tokenized_text, tokens):
-    tokens = list(tokens)
-    # 2-d matrix where ["some"]["string"] yield the cooccurrence of "some" and "string"
-    matrix = [[0 for token in tokens] for token in tokens] 
-    token_indexes = {token : tokens.index(token) for token in tokenized_text}
-    
-    sentences = [ tokenized_text[i:i + 2] for i in range(len(tokenized_text) - 1)]
-    for sentence in sentences: 
-        idx1 = token_indexes[sentence[0]]
-        idx2 = token_indexes[sentence[1]]
-        matrix[idx1][idx2] += 1
-
-    return matrix, token_indexes
+    stemmer = SnowballStemmer(language = 'english')
+    # Stemming makes it ugly, but may be required. Not for now though.
+    res = res.lower()
+    token_stream = [word for word in res.split(' ') if word not in stopwords and not word == '']
 
 
-def preprocess_corpus(corpus, stopwords):
-    tokens = set()
-    tokenized_texts = []
-
-    for text in corpus: 
-        # Gather all the tokens in the corpus
-        tokenized_text = tokenize(text, stopwords)
-        tokens.union(set(tokenized_text))
-    
-    return [get_attribute_vector(tokenized_text, tokens) for tokenized_text in tokenized_texts]
+    return token_stream
 
 
-test()
+def tokenize_corpus(corpus, stopwords):
+    token_streams = [tokenize(document, stopwords) for document in corpus]
+    token_set = set([token for token_stream in token_streams for token in token_stream])
+    return token_streams, token_set
