d761ea79c3952bc23ed08955a53ad4e4143454ec
minibrams <anders@brams.dk>
2018-12-06 10:06:48 +0100

    Added debugging prints to debug on server

--- KeywordExtraction/keyword_graph.py
+++ KeywordExtraction/keyword_graph.py
@@ -9,11 +9,22 @@ class TKGExtractor():
             a word graph over a corpus (list of texts as strings) of texts 
             and extract keywords from the graph. 
         '''
-        self.tokenize_corpus(corpus)
+        print(f'--------- INITIALIZING KEYWORD EXTRACTION -----------')
+        t_stream, t_all = self.tokenize_corpus(corpus)
+        print(f'TKGExtractor instance created with the following corpus properties: ')
+        print(f'    - Number of documents: {len(corpus)}')
+        num_words = [len(post) for post in corpus]
+        sum_words = sum(num_words)
+        print(f'    - Total words: {sum_words}')
+        avg_words = sum_words / len(corpus)
+        print(f'    - Avg. post length: {avg_words}')
+        print(f'    - Number of unique words: {len(t_all)}')
         
     def _tokenize_corpus(self, corpus): 
+        print(f'Tokenizing...')
         token_streams = [get_processed_text(text, lemmatize=True) for text in corpus]
-        tokens_all = set() 
+        print(f'Preprocessing completed, received token streams.')
+        tokens_all = set()
         for token_stream in token_streams: 
             tokens_all.update(set(token_stream))
         return token_streams, tokens_all
@@ -25,8 +36,10 @@ class TKGExtractor():
         self.token_set = token_set
 
     def build_graph(self):
+        print(f'Building graph...')
         graph = nx.Graph()
         graph.add_nodes_from(self.token_set)
+        print(f'Added {len(self.token_set)} nodes to graph...')
 
         for token_stream in self.token_streams: 
             # Create shingles for conducting all-neighbour-edging
@@ -40,11 +53,18 @@ class TKGExtractor():
                 for neighbour in shingle[1:]:
                     graph.add_edge(first_token, neighbour)
 
+        print(f'Added {len(graph.edges())} edges between nodes to graph...')
+
         return graph
 
     def extract_n_keywords(self, n = 5):
+        print(f'Extracting {n} keywords...')
         graph = self.build_graph()
+        print(f'Graph building completed.')
         closeness_scores = nx.closeness_centrality(graph)
+        print(f'Completed closeness centrality...')
         closeness_scores = sorted(closeness_scores.items(), key=operator.itemgetter(1))
+        print(f'Sorted closeness scores...')
         closeness_scores.reverse()
+        print(f'Returning keywords...')
         return [word for (word, closeness) in closeness_scores[:n]]
