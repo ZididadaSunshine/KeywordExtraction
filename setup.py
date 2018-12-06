0a1ed36023b883d50a5a96fbcedf2b97ec9052e0
Matias Jensen <1457902+OfficialHisha@users.noreply.github.com>
2018-12-06 11:23:42 +0100

    Updated version number
    Fixed an error (by Brams) that broke KWE completely and made all our work useless. Thanks Brams.......

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='KeywordExtraction',
-    version='1.1.2.a',
+    version='1.1.2',
     packages=find_packages(),
     url='https://github.com/ZididadaSunshine/KeywordExtraction',
     license='',
