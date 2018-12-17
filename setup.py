9b02a5d7e3895f6042223e16564cdb7bb21e3381
Anders Brams <anders@brams.dk>
2018-12-17 11:10:58 +0100

    Version 1.6

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='KeywordExtraction',
-    version='1.5',
+    version='1.6',
     packages=find_packages(),
     url='https://github.com/ZididadaSunshine/KeywordExtraction',
     license='',
